8643e2d77f9282af883f6352f34820c3739eb175
Manuel Araoz <manuelaraoz@gmail.com>
2020-11-29 23:23:29 -0300

    system D

--- main.py
+++ main.py
@@ -38,15 +38,19 @@ print('|B| = ' + str(len(B)))
 print('')
 
 def density_pattern(d):
-    l1 = int(d[0])
-    l2 = int(d[2])
-    d1 = 1
-    d2 = 1 if l1 == 1 else 2
-    d3 = 1 + (1 if l1 == 3 else 0) + (1 if l2 == 2 else 0)
-    return str(d1)+str(d2)+str(d3)
+    ret = ''
+    remaining = []
+    for note in [d[i:i+2] for i in range(0, len(d), 2)]:
+        for i in range(len(remaining)):
+            if remaining[i] > 0:
+                remaining[i] -= 1
+        di = note[0]
+        remaining.append(int(di))
+        ret += str(len([x for x in remaining if x > 0]))
+    return ret
 
 count = {}
-print('Density patterns:')
+print('B density patterns:')
 for design in B:
     density = density_pattern(design)
     print(design + ' -> ' + density)
@@ -70,3 +74,27 @@ for design in C:
     print(design + ' -> ' + reduce(design))
 
 # system D
+
+D = []
+
+for design in C:
+    for i in range(4):
+        for j in range(3):
+            for k in range(2):
+                D.append(str(i+1)+design[0]+str(j+1)+design[1]+str(k+1)+design[2]+"1"+design[3])
+print('D = '+' | '.join(D))
+print('|D| = ' + str(len(D)))
+print('')
+
+count = {}
+print('D density patterns:')
+for design in D:
+    density = density_pattern(design)
+    if density is None:
+        continue
+    print(design + ' -> ' + density)
+    count[density] = count.get(density, 0) + 1
+
+for k in count:
+    print(k + ' -> '+str(count[k])+' times.')
+
