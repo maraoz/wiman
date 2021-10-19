d4c4f9c2e50315b4877247bd05d35de804fdaf53
Manuel Araoz <287189+maraoz@users.noreply.github.com>
2021-10-18 23:50:34 -0500

    2+3 change sequence

--- change/main.py
+++ change/main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
 def state(chord):
   if len(chord)!=3:
     raise Exception("Chords must have 3 notes")
@@ -18,6 +20,29 @@ def note_to_letter(note):
 
 def chord_to_letters(chord): 
   return '-'.join([note_to_letter(note) for note in chord])
+
+def chord_fundamental(chord):
+  chord_state = state(chord)
+  low, mid, high = canonical(chord)
+  if chord_state == 0:
+    return low
+  if chord_state == 1:
+    return high
+  if chord_state == 2:
+    return mid
+
+STATE_NAME=['fnd', '1st', '2nd']
+def chord_to_name(chord):
+  chord_state = state(chord)
+  fundamental = chord_fundamental(chord)
+  return "("+note_to_letter(fundamental)+ ',' +STATE_NAME[chord_state]+')'
+
+GRADE_NAMES=['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
+GRADE_NAMES_VISUAL = [('o'*(i+1)) for i in range(7)]
+def chord_to_grade(chord, visual=True):
+  fundamental = chord_fundamental(chord)
+  names = GRADE_NAMES_VISUAL if visual else GRADE_NAMES
+  return names[fundamental-1]
 
 # returns 0 for fundamental, 1 for first inversion, 2 for second inversion
 def state(chord):
@@ -46,19 +71,23 @@ def change(chord, n):
   
   current = chord
   for _ in range(n):
-    current = change(current)
+    current = change_one(current)
   return current
 
-def pretty_print(chord, header=False, name=True, min_pitch=-6, max_pitch=8, columns=True):
+def pretty_print(chord, line=None, header=False, info=True, min_pitch=-6, max_pitch=8, columns=True, extra=''):
   if header:
+    if line is not None:
+      print('\t', end='')
     for pitch in range(min_pitch, max_pitch+1):
       print(note_to_letter(pitch),end='')
     print('')
+  if line is not None:
+    print(str(line)+":", end='\t')
   for note in range(min_pitch,max_pitch+1):
     back = '|' if (columns and note_to_letter(note)=='C') else ' '
     print('o' if note in chord else back, end = '')
 
-  print('' if not name else (" "+chord_to_letters(chord)))
+  print('' if not info else ("|\t"+ chord_to_grade(chord)+"\t"+chord_to_letters(chord)+"\t"+chord_to_name(chord)+" \t"+extra))
 
 c_chord_0 = canonical([1, 3, 5])
 print("fundamental C chord", c_chord_0, chord_to_letters(c_chord_0))
@@ -75,7 +104,8 @@ pretty_print(c_chord_2)
 c_changed_one = change_one(c_chord_0)
 print(c_changed_one, state(c_changed_one))
 
-print("change=1 loop")
+print("="*80)
+print("1 change loop")
 current = c_chord_0
 finished = False
 for row in range(30):
@@ -84,4 +114,40 @@ for row in range(30):
     current = change_one(current)
     if chord_to_letters(current) == chord_to_letters(c_chord_0):
       finished = True
-  pretty_print(current, header=(row%10==0), max_pitch=15)
+  pretty_print(current, header=(row%7==0), max_pitch=15)
+
+print("="*80)
+print("2+3 change sequence loop")
+current = c_chord_0
+finished = False
+for row in range(500):
+  if (finished): break
+
+  current_change = 2 if (row%2==1) else 3
+  next_change = 2 if ((row+1)%2==1) else 3
+  
+  if (row != 0):
+    current = change(current, current_change)
+    if chord_to_letters(current) == chord_to_letters(c_chord_0) and next_change == 2:
+      finished = True
+  
+  pretty_print(current, line=row, header=(row%1000==0), min_pitch=1, max_pitch=15+7*4, extra=('+'+str(next_change)))
+
+sys.exit(0)
+
+print("="*80)
+print("1+2+3 change sequence loop")
+current = c_chord_0
+finished = False
+for row in range(30):
+  if (finished): break
+
+  current_change = 1+((row-1)%3)
+  next_change = 1+((row)%3)
+  
+  if (row != 0):
+    current = change(current, current_change)
+    if chord_to_letters(current) == chord_to_letters(c_chord_0) and next_change == 1:
+      finished = True
+  
+  pretty_print(current, line=row, header=(row%1000==0), min_pitch=1, max_pitch=15+7*4, extra=('+'+str(next_change)))
